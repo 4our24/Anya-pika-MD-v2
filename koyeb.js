5bbffbc0438a4ff11f954dbaa23f3a2a2b28352d
4our24 <123201123+4our24@users.noreply.github.com>
2023-01-22 22:25:41 +0200

    Update koyeb.js

--- koyeb.js
+++ koyeb.js
@@ -7,9 +7,9 @@ RUN sudo dnf -y update &&\
 
 RUN git clone https://github.com/PikaBotz/Anya-pika-MD-v2
 
-WORKDIR /root/Anya-pika-MD-v2
+WORKDIR /root/Jiraya-Four-MD-v2
 
-COPY ./root/Anya-pika-MD-v2
+COPY ./root/Jiraya-Four-MD-v2
 
 RUN yarn install
 
